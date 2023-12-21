e911f1919aa03e8f654286325a457bb87b42d62f
erfan mahmoudi <erfanmahmudi1383@gmail.com>
2023-12-21 18:13:10 +0330

    PID libraries added

--- DC_Motor_With_L298N.ino
+++ DC_Motor_With_L298N.ino
@@ -1,249 +1,233 @@
 #include <Servo.h>
+#include "Sensors.h"
+#include "Motors.h"
+#include "PID.h"
 
 // create servo objects to control servo motors
 Servo rgbSensorServo;
 Servo liverageServo;
 Servo gripperServo;
+// ==================================== SETTINGS ==================================/////////////////
+const int THRESHOLD = 5;
+const int SENSORS_COUNT = 4;
+const int SENSOR_PINS[SENSORS_COUNT] = {12, 11, 4, 3};
+const int ERRORS[4] = {0, 1, 2, 3};
+
+const int MAX_SPEED = 255;
+const int NORMAL_SPEED = 205;
+const int MIN_SPEED = 0;
+
+const int LEFT_IN1 = 9;
+const int LEFT_IN2 = 8;
+const int LEFT_EN = 10;
+
+const int RIGHT_IN1 = 7;
+const int RIGHT_IN2 = 6;
+const int RIGHT_EN = 5;
 
 //UltraSonics' Pins
-int front_trigPin = 12;//green
-int front_echoPin = 11;//blue
-int right_trigPin = 4;
-int right_echoPin = 3;
+const int FRONT_TRIG = 12;
+const int FRONT_ECHO = 11;
+const int RIGHT_TRIG = 4;
+const int RIGHT_ECHO = 3;
 
+const float KP = 80;
+const float KI = 50;
+const float KD = 0;
+
+const bool DEBUG = false;
+const int DELAY_TIME = 1000;
+
+// ==============================================  /////////////////
 //variable to store the servo motors position
 int rgbSensorpos = 0;
 int liveragepos = 0;
 int gripperpos = 0;
 
-// connect motor controller pins to Arduino digital pins
-// motor one
-int enA = 10;
-int in1 = 9;
-int in2 = 8;
-// motor two
-int enB = 5;
-int in3 = 7;
-int in4 = 6;
+Sensors sensors(SENSORS_COUNT, SENSOR_PINS, ERRORS);
+
+Motors motors(
+    NORMAL_SPEED, MIN_SPEED, MAX_SPEED,
+    LEFT_IN1, LEFT_IN2, LEFT_EN,
+    RIGHT_IN1, RIGHT_IN2, RIGHT_EN
+);
+PID pid(KP, KI, KD);
 
 void setup()
 {
   Serial.begin(9600);
-  //Ultrasonic Sensor details set
-  pinMode(front_trigPin , OUTPUT);
-  pinMode(front_echoPin , INPUT);
-  pinMode(right_trigPin , OUTPUT);
-  pinMode(right_echoPin , INPUT);
-  // set all the motor control pins to outputs
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  motors.initialize();
+  sensors.initialize();
+
   //set servo pins on arduino board
-  rgbSensorServo.attach(0);
-  liverageServo.attach(1);
-  gripperServo.attach(2);
+  // rgbSensorServo.attach(0);
+  // liverageServo.attach(1);
+  // gripperServo.attach(2);
 }
-void demoOne()
-{
-  // this function will run the motors in both directions at a fixed speed
-  // turn on motor A
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  // set speed to 200 out of possible range 0~255
-  analogWrite(enA, 200);
-  // turn on motor B
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  // set speed to 200 out of possible range 0~255
-  analogWrite(enB, 200);
-  delay(2000);
-  // now change motor directions
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH); 
-  delay(2000);
-  // now turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}
-void demoTwo()
-{
-  // this function will run the motors across the range of possible speeds
-  // note that maximum speed is determined by the motor itself and the operating voltage
-  // the PWM values sent by analogWrite() are fractions of the maximum speed possible 
-  // by your hardware
-  // turn on motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH); 
-  // accelerate from zero to maximum speed
+void loop() {
+  int error = sensors.calculate_error();
+  int speed_difference = pid.calculate_speed_difference(error);
+  motors.drive(speed_difference, DEBUG);
 
-  for (int i = 0; i < 256; i++) { 
-    analogWrite(enA, i); 
-    analogWrite(enB, i); 
-    delay(20); 
-  } 
-  // decelerate from maximum speed to zero 
-  for (int i = 255; i > 0; --i) {
-    analogWrite(enA, i);
-    analogWrite(enB, i);
-    delay(20);
-  } 
- 
-  // now turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);  
-}
-void goForward(){
-  //turn on motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  // accelerate from zero to maximum speed
- for (int i = 0; i < 256; i++) { 
-    analogWrite(enA, i); 
-    analogWrite(enB, i); 
-    delay(20); 
-  } 
-
-}
-//turn to right direction
-void goRight(){
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH); 
-  for (int i = 0; i < 50 ; i++){
-    analogWrite(enB, i);
-    delay(20);
+  if(DEBUG) {
+      Serial.print("error: ");
+      Serial.println(error);
+      Serial.print("speed_difference: ");
+      Serial.println(speed_difference);
+      Serial.println("--------------------------------------");
+      delay(DELAY_TIME);
   }
-}
-//turn to left direction
-void goLeft(){
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);  
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW); 
-  for (int i = 0; i < 50 ; i++){
-    analogWrite(enA, i);
-    delay(20);
-  }
-}
-//Break
-void stop(){
-  // decelerate from maximum speed to zero 
-  for (int i = 255; i > 0; --i){
-    analogWrite(enA, i);
-    analogWrite(enB, i);
-    delay(20);
-  } 
-  // now turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);  
-}
-
-void loop()
-{
   // demoOne();
   // delay(1000);
   // demoTwo();
   // delay(1000);
-  long duration , distance;
+  // int duration , distance;
   //sending trigger pulse to signal
-  triggerSignal(front_trigPin);
-  triggerSignal(right_trigPin);
-
-  int front_distance = getDistance(front_echoPin);
-  int right_distance = getDistance(right_echoPin);
+  // digitalWrite(front_trigPin , LOW);
+  // delayMicroseconds(2);
+  // digitalWrite(front_trigPin , HIGH);
+  // delayMicroseconds(10);
+  // digitalWrite(front_trigPin , LOW);
+  // triggerSignal(right_trigPin);
+  // duration = pulseIn(front_echoPin, HIGH);
+  // int front_distance = (duration / 2) / 29.1;
+  // int right_distance = getDistance(right_echoPin);
   //Decision
-  if (front_distance >= 15 && right_distance <= 5){
-    Serial.println("going forward...");
-    goForward();
-  }
-  else if (right_distance > 6 && right_distance < 10){
-    Serial.println("set distance from right");
-    goRight();
-    delay(20);
-    goLeft();
-    Serial.print("distance from right : ");
-    Serial.print(getDistance(right_echoPin));
-    Serial.println(" cm");
-  }
-  else if (right_distance > 11){
-    Serial.println("going to right...");
-    for (int i = 0 ; i < 4; i++){
-      goRight();
-      delay(20);
-    }
-  }
-  else if (front_distance < 14){
-    Serial.println("going for stop...");
-    stop();
-    delay(20);
-  }
+  // if (front_distance >= 15){
+  //   goForward();
+  // }
+  // else{
+  //   stop();
+  // }
+  // else if (right_distance > 6 && right_distance < 10){
+  //   Serial.println("set distance from right");
+  //   goRight();
+  //   delay(20);
+  //   goLeft();
+  //   Serial.print("distance from right : ");
+  //   Serial.print(getDistance(right_echoPin));
+  //   Serial.println(" cm");
+  // }
+  // else if (right_distance > 11){
+  //   Serial.println("going to right...");
+  //   for (int i = 0 ; i < 4; i++){
+  //     goRight();
+  //     delay(20);
+  //   }
+  // }
+  // else if (front_distance < 14){
+  //   Serial.println("going for stop...");
+  //   stop();
+  //   delay(20);
+  // }
   //helps to control the rate at which distance measurements are taken
-  delay(500);
+  delay(1);
 }
-float getDistance(int given_echoPin){
-  float duration = pulseIn(given_echoPin, HIGH);
-  float distance = (duration / 2) / 29.1;
-  return distance;
-}
-void triggerSignal(int given_trigPin){
-  digitalWrite(given_trigPin , LOW);
-  delayMicroseconds(2);
-  digitalWrite(given_trigPin , HIGH);
-  delayMicroseconds(10);
-  digitalWrite(given_trigPin , LOW);
-}
-void gripIn(){
-  for (pos = 0; pos <= 20; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 20; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-}
-void gripOut(){
+// void demoOne()
+// {
+//   // this function will run the motors in both directions at a fixed speed
+//   // turn on motor A
+//   digitalWrite(in1, HIGH);
+//   digitalWrite(in2, LOW);
+//   // set speed to 200 out of possible range 0~255
+//   analogWrite(enA, 200);
+//   // turn on motor B
+//   digitalWrite(in3, HIGH);
+//   digitalWrite(in4, LOW);
+//   // set speed to 200 out of possible range 0~255
+//   analogWrite(enB, 200);
+//   delay(2000);
+//   // now change motor directions
+//   digitalWrite(in1, LOW);
+//   digitalWrite(in2, HIGH);  
+//   digitalWrite(in3, LOW);
+//   digitalWrite(in4, HIGH); 
+//   delay(2000);
+//   // now turn off motors
+//   digitalWrite(in1, LOW);
+//   digitalWrite(in2, LOW);  
+//   digitalWrite(in3, LOW);
+//   digitalWrite(in4, LOW);
+// }
+// void demoTwo()
+// {
+//   // this function will run the motors across the range of possible speeds
+//   // note that maximum speed is determined by the motor itself and the operating voltage
+//   // the PWM values sent by analogWrite() are fractions of the maximum speed possible 
+//   // by your hardware
+//   // turn on motors
+//   digitalWrite(in1, LOW);
+//   digitalWrite(in2, HIGH);  
+//   digitalWrite(in3, LOW);
+//   digitalWrite(in4, HIGH); 
+//   // accelerate from zero to maximum speed
 
-}
-void pullUp(){
+//   for (int i = 0; i < 256; i++) { 
+//     analogWrite(enA, i); 
+//     analogWrite(enB, i); 
+//     delay(20); 
+//   } 
+//   // decelerate from maximum speed to zero 
+//   for (int i = 255; i > 0; --i) {
+//     analogWrite(enA, i);
+//     analogWrite(enB, i);
+//     delay(20);
+//   } 
+ 
+//   // now turn off motors
+//   digitalWrite(in1, LOW);
+//   digitalWrite(in2, LOW);  
+//   digitalWrite(in3, LOW);
+//   digitalWrite(in4, LOW);  
+// }
 
-}
-void pullDown(){
+//turn to right direction
+// void goRight(){
+//   digitalWrite(in1, LOW);
+//   digitalWrite(in2, HIGH);  
+//   digitalWrite(in3, LOW);
+//   digitalWrite(in4, HIGH); 
+//   for (int i = 0; i < 50 ; i++){
+//     analogWrite(enB, i);
+//     delay(20);
+//   }
+// }
+//turn to left direction
+// void goLeft(){
+//   digitalWrite(in1, HIGH);
+//   digitalWrite(in2, LOW);  
+//   digitalWrite(in3, HIGH);
+//   digitalWrite(in4, LOW); 
+//   for (int i = 0; i < 50 ; i++){
+//     analogWrite(enA, i);
+//     delay(20);
+//   }
+// }
+//Break
 
-}
-void rotateUp(){
+// void gripIn(){
+//   for (pos = 0; pos <= 20; pos += 1) { // goes from 0 degrees to 180 degrees
+//     // in steps of 1 degree
+//     myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//     delay(15);                       // waits 15ms for the servo to reach the position
+//   }
+//   for (pos = 20; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+//     myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//     delay(15);                       // waits 15ms for the servo to reach the position
+//   }
+// }
+// void gripOut(){
 
-}
-void rotateDown(){
+// }
+// void pullUp(){
 
-}
+// }
+// void pullDown(){
 
+// }
+// void rotateUp(){
 
+// }
+// void rotateDown(){
 
-
-
-
-
-
-
-
-
-
-
+// }
